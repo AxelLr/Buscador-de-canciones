cabd4d8f003317faf2c5acb9e305e4d698167149
Axel <axelleonr12@gmail.com>
2019-12-24 21:24:25 -0300

    Updates

---
+++ precache-manifest.61fc5a7fb3604e6c709b1c41311bd889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0985eaa35eadaf923a9b9b75f5740d",
+    "url": "/Buscador-de-canciones/index.html"
+  },
+  {
+    "revision": "402a0af94f2ad110c229",
+    "url": "/Buscador-de-canciones/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "606070f4dfd0a88f4ccd",
+    "url": "/Buscador-de-canciones/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "402a0af94f2ad110c229",
+    "url": "/Buscador-de-canciones/static/js/2.57dddea5.chunk.js"
+  },
+  {
+    "revision": "86a1c49faaaa8cf57a89ea611511cb42",
+    "url": "/Buscador-de-canciones/static/js/2.57dddea5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "606070f4dfd0a88f4ccd",
+    "url": "/Buscador-de-canciones/static/js/main.5ce509bb.chunk.js"
+  },
+  {
+    "revision": "bcb783e7cc6f205df031",
+    "url": "/Buscador-de-canciones/static/js/runtime-main.628eea3e.js"
+  }
+]);
